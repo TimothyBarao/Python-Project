52e6bbc414f8d0390d24c0680d7c09197fb834be
Timothy Barao <timothy.barao@gmail.com>
2016-06-22 23:33:55 -0400

    Delete test1.py

--- Test programs/test1.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-song = AudioSegment.from_file( "Music/erb.mp3",  format="mp3")
-
-half = len(song) / 2
-first_half = song[:half]
-sh3 = first_half + first_half + first_half
-
-sh3.export("Music/3test.mp3", format="mp3")
-print "Export complete"
