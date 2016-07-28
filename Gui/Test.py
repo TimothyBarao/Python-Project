d11869cc961d9818ec7184d0b93e8d03be8d135e
Timothy Barao <timothy.barao@gmail.com>
2016-07-27 23:28:18 -0400

    Gui/Waveform Testing
    Combines Waveform creation with Amen testing, uses .wav files as input, creates third mash-up song, and generates three waveforms, one for each song

--- Gui/Test.py
+++ Gui/Test.py
@@ -1,41 +1,79 @@
+from pylab import *
+from amen.utils import example_audio_file
+from amen.audio import Audio
+from amen.synthesize import synthesize
+
 import sys
 import numpy as np
-from pylab import *
 import scipy.io.wavfile
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import wave
 import copy
 
+def mash(song1, song2):
+    audio1 = Audio(song1)
+    audio2 = Audio(song2)
+
+    beats1 = audio1.timings['beats']
+    beats2 = audio2.timings['beats']
+
+    mashup =[]
+    i = 1
+    for beat1, beat2 in zip(beats1, beats2):
+	    if(i%8 == 7):
+		    mashup.append(beat1)
+	    else:
+		    mashup.append(beat2)
+	    i+=1
+
+    out = synthesize(mashup)
+    output = 'one_knob.wav'
+    out.output(output)
+    return output
 
 def create_wave(song):
-    #np.set_printoptions(threshold=np.inf)
     mpl.rcParams['agg.path.chunksize'] = 10000
     rate, data = scipy.io.wavfile.read(song)
     data = copy.deepcopy(data)
     newdata =np.array([[0,0]])
-    blerg = np.array([[0,0]])
-    print(data.shape)
-    print(data.shape[0])
-    
+
     i = 0
     while 1:
         newdata = np.append(newdata, data[i])
-        i += 8000
+        i += 10000
         if i > data.shape[0]:
             break
-        
-    print(data.shape)
-    print(newdata)
-    
+  
+    return newdata
 
+if __name__ == '__main__':
+    try:
+        song1 = "wav/knob.wav"
+        song2 = "wav/time.wav"
+        song3 = mash(song1, song2)
 
+        song1_plot = create_wave(song1)
+        song2_plot = create_wave(song2)
+        song3_plot = create_wave(song3)
 
-    #x = np.arange(0.0, 2.0, 0.01)
-    #data = np.sin(data*x)
-    
-    plt.plot(newdata)   
-    plt.show()
+        fig = plt.figure()
+      
+        ax = plt.subplot(311)
+        ax.set_title(song1)
+        ax.plot(song1_plot)
 
-song = sys.argv[1]
-create_wave(song)
+        ax = plt.subplot(312)
+        ax.set_title(song2)
+        ax.plot(song2_plot)
+
+        ax = plt.subplot(313)
+        ax.set_title(song3)
+        ax.plot(song3_plot)
+
+        plt.show()
+
+    except ValueError:
+        print('IT ERRORED')
+        #mash(audio_file2, audio_file1)
+
